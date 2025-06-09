1a75bb5ea2391d34571a8b53a4032e6710d1e857
Raphael Fagundes <rphagundes@gmail.com>
2025-06-09 07:00:48 -0300

    Creating menu ang game code Main loop connecting Menu and Game Classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pygame as pyg
+
+from code.menu import Menu # Import the pygame library as 'pyg'
+
 class Game:
     def __init__(self):
-        self.window = None
+        #EU COLOCO PYG.INIT E SETUP DA JANELA DENTRO DO CONSTRUTOR DA CLASSE, POIS QUERO QUE SEJA FEITO APENAS UMA VEZ E NÃO REESCRITO A CADA LOOP DO JOGO
+        pyg.init() # Initialize the pygame library
+        self.window = pyg.display.set_mode (size=(800,600)) # Create a window with a size of 800x600 pixels -> size deve ser criada em tupla
 
     def run(self, ):
-        pass
+       
+        while True: # Start an infinite loop
+            menu = Menu(self.window)
+            menu.run() #execute the run method of the Menu class
+            pass
+
+
+
+
+
+            # Check for all events
+            # for event in pyg.event.get():
+            #     if event.type == pyg.QUIT:
+            #         pyg.quit() #if the event is a quit event, exit the loop == close the window
+            #         quit() #End PYGAME
+
+
+
+
+
+ 
+
+
+
+
+
