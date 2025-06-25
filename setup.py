9bd0148d6bf5a372d9d5e73769bc49f0d525d223
Raphael Fagundes <rphagundes@gmail.com>
2025-06-24 21:46:17 -0300

    Fix comments formatting and improve cx_Freeze setup in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from cx_Freeze import setup, Executable
 import os
 
-#read all asset list to include on .exe
+# Read all asset list to include on .exe
 path = "./asset"
 asset_list = os.listdir(path)
 asset_list_completa = [os.path.join(path, asset).replace("\\", "/") for asset in asset_list]
 print(asset_list_completa)
 
-
-#cs_Freeze code
-executables = [Executable("main.py")]
+# cx_Freeze code
+executables = [
+    Executable(
+        "main.py",
+        icon="asset/icon.png"  # Set your icon here
+    )
+]
 files = {"include_files": asset_list_completa, "packages": ["pygame"]}
 
 setup(
     name="SpaceShooter",
     version="1.0",
     description="Space Shooter app",
-    options={"build_exe": files}, #options={"build_exe":{"packages": ["pygame"]}
+    options={"build_exe": files},
     executables=executables
-    
 )
